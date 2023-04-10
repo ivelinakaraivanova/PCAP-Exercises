7523af3a8e92701baa9c059b0596f93784553a78
ivelinakaraivanova <73067985+ivelinakaraivanova@users.noreply.github.com>
2023-04-10 16:15:17 +0300

    Update misc.py

--- src/Misc/misc.py
+++ src/Misc/misc.py
@@ -103,3 +103,73 @@ class X:
 print(hasattr(X, 'z'))
 '''
 
+'''
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+
+    def get_sum(self):
+        return self.__sum
+
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+
+
+stack_object = AddingStack()
+
+for i in range(5):
+    stack_object.push(i)
+print(stack_object.get_sum())
+
+for i in range(5):
+    print(stack_object.pop())
+'''
+
+'''
+class Ex(Exception):
+    def __init__(self, msg):
+        super().__init__(msg+msg)
+        self.args = (msg,)
+
+try:
+    raise Ex('ex')
+except Ex as e:
+    print(e)
+except Exception as e:
+    print(e)
+'''
+
+'''
+class A:
+    def __init__(self):
+        pass
+
+a = A(1)
+print(hasattr(a, 'A'))
+'''
+
+class A:
+    A = 1
+
+
+print(hasattr(A, "A"))
